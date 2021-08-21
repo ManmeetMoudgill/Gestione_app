255594c55a2d901c63b2e237fd2aaa587997ce3a
Manmeet Moudgill <manmeetmoudgill76@gmail.com>
2021-08-21 23:45:42 +0200

    Files Added

--- src/Components/Gruppi.jsx
+++ src/Components/Gruppi.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import Header from './Header'
+import React,{useEffect,useState} from "react";
+import Header from "./Header";
 function Gruppi() {
-    return (
+    const [dataSelect,setDataSelect]=useState([])
+    useEffect(()=>{
+        
+        const url="http://localhost/WorkProject/Gruppi.php";
 
-        <div className="Gruppi__page">
-            <Header/>
-            <h2>This is the Grouppi Page</h2>
-        </div>
-    )
+        fetch(url,{mode: 'cors'}).then((res)=>{
+            return res.json();
+
+        }).then((data)=>{
+           
+            setDataSelect(data)
+        
+            
+           
+        }).catch((err)=>console.log(err))
+    },[])
+
+  return (
+      <>
+      <Header />
+    <div className="Gruppi__page border-2 border-red-500">
+
+      {/* Upper div */}
+      <div className="upper__div flex flex-col items-center justify-center">
+       <label className="text-2xl">Inserisci Nome Gruppo</label>
+      
+
+       <select id="select" className="border-2  w-60 mt-2.5 border-black-700" name="groups" id="">
+        <option value="">Scegli il Gruppo</option>
+          
+      {dataSelect.map((data,id)=>{
+           return <option className="text-2xl" key={id}  value={data.id_gruppo} >{data.nome}</option>
+           
+          
+        })}
+        </select>
+      </div>
+
+      {/* Bottom Div */}
+      <div className="bottom__div">
+
+      </div>
+    </div>
+    </>
+  );
 }
 
-export default Gruppi
+export default Gruppi;
